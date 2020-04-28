46933e9de8e99336c209353f59653896772a9aad
ulfat-dawood <ulfat_dawood@outlook.com>
2020-04-28 08:10:50 +0530

    Creating Schema

---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+const mongoose= require('mongoose');
+ 
+const Schema = mongoose.Schema; 
+
+const userSchema= new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastName: {
+        type:String,
+        maxlength: 32,
+        trim : true
+    },
+    email:{
+        type:String,
+        required: true, 
+        trim: true, 
+        unique: true
+    },
+    userinfo:{
+        type : String,
+        trim: true
+    },
+    password:{
+        type:String,
+        trim:true
+    },
+    salt: String,
+    role:{      //defining preveliges 
+        type:Number,
+        default:0    //0 indicate basic user
+    },
+    purchases:{
+        type: Array,
+        default: []
+    }
+})
+
+module.exports= mongoose.model('user', userSchema, 'users')
