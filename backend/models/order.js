88d5fa5e1065dba1209c47b961107cb4a2f9ab40
ulfat-dawood <ulfat_dawood@outlook.com>
2020-05-19 15:40:43 +0530

    Schema Completed

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,9 +1,31 @@
 const mongoose= require('mongoose');
 const Schema, {ObjectId}= mongoose.Schema;
 
+const ProductCartSchema= new Schema({
+    product:{
+        type: ObjectId,
+        ref: 'Product'
+    },
+    name: String,
+    count: Number,
+    price: Number
+})
+
+const ProductCart= mongoose.model('OrdProductCarter', ProductCartSchema); 
+
 const orderSchema= new Schema(
     {timestamps: true},
     {
-
+        products: [ProductCartSchema],//array of (products in the order) schema
+        transaction_id: {},
+        amount: Number ,
+        address: String ,
+        updated: Date, 
+        user: {
+            type: ObjectId,
+            ref: 'User'
+        }
     }
 )
+
+const Order= mongoose.model('Order', orderSchema); 
