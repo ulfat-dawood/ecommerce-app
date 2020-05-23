d1963fe65d62c2bfb234df7a73cde5a5e58afd99
ulfat-dawood <ulfat_dawood@outlook.com>
2020-05-23 15:07:48 +0530

    middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const mongoose= require('mongoose'); 
+const bodyParser= require('body-parser'); 
+const cookieParser= require('cookie-parser'); 
+const cors= require('cors');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8000; 
+
+mongoose.connect(process.env.DATABASE,  
+    { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true //to use indexing
+}).then(()=>{
+    console.log('mognoDB connected');
+})
+
 
 app.get('/hello', (req,res)=> res.send('Hello Express')); 
 app.listen(port, () => console.log(`listening at port:${port}`))
