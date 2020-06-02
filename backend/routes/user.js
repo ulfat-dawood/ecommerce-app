b69786f244bc5dec7e132e5f0b54d07e2907dd46
ulfat-dawood <ulfat_dawood@outlook.com>
2020-06-02 19:00:41 +0530

    getUserById using params

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,16 @@
 const User= require('../models/user'); 
+
+
+exports.getUserById = (req, res, next, id)=>{
+    User.findById(id).exec((err, user)=>{
+        if(err || !user){ //if error or no user was found
+            return res.status(400).json({
+                error: 'USER NOT FOUND'
+            })
+        }
+        //if no err and user found, store the user in th fronend 'profile' variable
+         req.profile= user; 
+         next(); 
+    })
+
+}
