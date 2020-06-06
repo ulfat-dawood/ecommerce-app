c096a127d7f21ecda443bac1c65dec96f2e6045b
ulfat-dawood <ulfat_dawood@outlook.com>
2020-06-06 14:48:35 +0530

    get all users route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -100,7 +100,7 @@ exports.isSignedIn= expressJwt({
 exports.isAuthenticated= (req, res, next)=>{
     //in the frontend create a property 'profile' that is sent with the req
     //first check if profile & auth exist then check if _id matches 
-    let checker= req.profile && req.auth && req.profile._id == req.auth._id; //auth is coming from isSignedIn() 
+    let checker= req.profile && req.auth && req.profile._id == req.auth.id; //auth is coming from isSignedIn()  
     if(!checker){
         return res.status(403).json({
             error: 'ACCESS DENIED'
@@ -109,7 +109,7 @@ exports.isAuthenticated= (req, res, next)=>{
     next();
 }
 
-exports.isAmin= (req, res, next)=>{
+exports.isAdmin= (req, res, next)=>{
 
     if(req.profile.role === 0){ 
         return res.status(403).json({
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,11 +9,27 @@ exports.getUserById = (req, res, next, id)=>{
             })
         }
         //if no err and user found, store the user in th fronend 'profile' variable
+        //TODO: how is this shit working 
          req.profile= user; 
          next(); 
     })
 }
 
 exports.getUser= (req, res)=>{
+    //for sercurity purposes, we will ommit the confidential information from the response
+    req.profile.salt= undefined; 
+    req.profile.encry_password= undefined; 
+    //when the value is undefined, it won't even show in the respons object
     return res.json(req.profile);
+}
+
+exports.getAllUsers= (req,res)=>{
+    User.find().exec((err, users)=>{
+        if(err || !users){
+            return res.status(400).json({
+                error: 'Users not found'
+            })
+        }
+        res.json(users);
+    })
 }
