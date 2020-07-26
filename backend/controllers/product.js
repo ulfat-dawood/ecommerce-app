f056bead974aa5df7b621b4be0121baa68ad33e9
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-27 03:06:09 +0530

    populate product with category

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,7 +1,9 @@
 const Product = require('../models/product'); 
 
 exports.getProductById= (req, res, next, id)=>{
-    Product.findById(id).exec((err, product)=>{
+    Product.findById(id)
+    .populate('category')
+    .exec((err, product)=>{
         if(err){
             return res.status(400).json({
                 error: 'Product not found'
