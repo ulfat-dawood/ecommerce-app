3c289723c6529635249de35fd28959083b03342a
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-28 02:34:53 +0530

    updatedProduct implemented

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -100,5 +100,43 @@ exports.deleteProduct= (req, res)=>{
 }
 
 exports.updateProduct= (req, res)=>{
+    //create a form: 
+    let form= new formidable.IncomingForm();
+    form.keepExtensions= true; 
 
+    form.parse(req, (err, fields, file)=>{
+        if(err){
+            return res.status(400).json({
+                error: "error occured while updating"
+            })
+        }
+
+        
+        
+        let product= req.product; 
+        //lodash's extend(): i think it merge the values of two obejct
+        //                   or if values already exist in the send object, it updates the values of the first object from the second
+        product= _.extend(product,fields)
+
+        if(file.photo){
+            if(file.photo.size > 3000000){ // > 3 MB
+                return res.status(400).json({
+                    error: 'file size is too big'
+                })
+            }
+            product.photo.data= fs.readFileSync(file.photo.path);
+            product.photo.contentType= file.photo.type
+        }
+
+        //store in DB: 
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({
+                    error: "unable to store product to DB"
+                })
+            }
+            res.json(product); 
+
+        })
+    })
 }
