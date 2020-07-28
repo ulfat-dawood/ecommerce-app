77d19d140339612a1faacf29e61435620eff0172
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-29 00:06:12 +0530

    getOrderByID implemeted

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,6 +1,16 @@
-const Order = require('../models/order'); 
+const { Order, ProductCart } = require('../models/order'); 
 
 
-exports.getOrderByID= (req, res, next, id)=>{
-    Order.f
+exports.getOrderByID= (req, res, next, idFromParam)=>{
+    Order.findById(idFromParam)
+    .populate('Products.product', 'name price')
+    .exec((err,order)=>{
+        if(err){
+            return res.status(400).json({
+                error: 'can\'t fetch orders' 
+            })
+        }
+        req.order= order; 
+        next(); 
+    })
 }
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,13 +2,13 @@ const express= require('express');
 const router= express.Router(); 
 
 const{getOrderByID}= require('../controllers/order'); 
-const{updateStock}= require('../controllers/product');
+//const{updateStock}= require('../controllers/product');
 const {isSignedIn, isAuthenticated, isAdmin}= require('../controllers/auth');
 const {getUserById, pushOrderInPurchaseList}= require('../controllers/user');
 
 //param middlwares:
 router.param('orderId', getOrderByID); 
 router.param('userId', getUserById);
-router.param('peroductId', getProductById)
+//router.param('productId', getProductById)
 
 module.exports= router; 
