ff0219ac838ea829ad2054f269abefc257ab3c9b
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-28 23:20:18 +0530

    importing all required contollers

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,11 +2,13 @@ const express= require('express');
 const router= express.Router(); 
 
 const{getOrderByID}= require('../controllers/order'); 
+const{updateStock}= require('../controllers/product');
 const {isSignedIn, isAuthenticated, isAdmin}= require('../controllers/auth');
-const {getUserById}= require('../controllers/user');
+const {getUserById, pushOrderInPurchaseList}= require('../controllers/user');
 
 //param middlwares:
 router.param('orderId', getOrderByID); 
-
+router.param('userId', getUserById);
+router.param('peroductId', getProductById)
 
 module.exports= router; 
