97419ea82ca95660d62c5019981b7dee5652f0d7
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-29 00:53:28 +0530

    order status

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -13,6 +13,10 @@ router.param('userId', getUserById);
 //actual routes: 
 router.post('/order/create/:userId',isSignedIn,isAuthenticated,pushOrderInPurchaseList, createOrder ); 
 
-//here we need :userId to pass req.profile to the authentocation middlewares: 
-router.get('/order/all/:userId',isSignedIn,isAuthenticated,isAdmin, getAllOrders)
+       //here we need :userId to pass req.profile to the authentocation middlewares: 
+router.get('/order/all/:userId',isSignedIn,isAuthenticated,isAdmin, getAllOrders);
+
+router.get('/order/status/:userId');
+        //admin will update the status: 
+router.put('/order/:orderId/status/:userId')
 module.exports= router; 
