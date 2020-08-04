2e9cbe38818e61669281d0e3b23fdb86b5b326b5
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-05 02:58:01 +0530

    routes.js

---
+++ frontend/src/Routes.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {Route, BrowserRouter as Router, Switch,Link } from 'react-router-dom'; 
+
+export default function Routes(){ 
+
+	return(
+	<Router>
+        <Switch>
+            <Route exact path='/' component={}/>
+        </Switch>
+    </Router>
+	) 
+}
