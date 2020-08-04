9693fd3c5c06d878d0d26b215fc9951bdb014c2a
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-05 03:08:38 +0530

    Homepage

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import {Route, BrowserRouter as Router, Switch,Link } from 'react-router-dom'; 
 
+import Home from './core/Home'
+
 export default function Routes(){ 
 
 	return(
 	<Router>
         <Switch>
-            <Route exact path='/' component={}/>
+            <Route exact path='/' component={Home}/>
         </Switch>
     </Router>
 	) 
