8b1aef9d92289ee59be490402b0c469aff10f5e4
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-05 15:39:44 +0530

    setting up env variables

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import '../styles.css';
 
 export default function App(){ 
+    console.log('env test:', process.env.REACT_APP_BACKEND_URL)
 
 	return(
 	<h1 className='text-white'>HOME</h1>
