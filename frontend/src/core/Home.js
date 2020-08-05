3f4bab5e194215ca0a1aa34994a1fd5c3c784b46
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-05 17:21:14 +0530

    basic Homepage

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -2,10 +2,25 @@
 import React from 'react';
 import '../styles.css';
 import {backendUrl} from '../backend' 
+import Base from './Base'; 
 
 export default function App(){ 
     
 	return(
-    <h1 className='text-white'>HOME is: {backendUrl}</h1>
+     <Base title='home page'>
+         <div className="row">
+             <div className="col-4">
+                 <button className="btn btn-success">Test</button>
+             </div>
+             <div className="col-4">
+                 <button className="btn btn-success">Test</button>
+             </div>
+             <div className="col-4">
+                 <button className="btn btn-success">Test</button>
+             </div>
+            
+
+         </div>
+     </Base>
 	) 
 }
