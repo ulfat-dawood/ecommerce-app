a895b2ec4d70ecbb85fcbc094cf3a103716d9dbb
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-06 13:41:26 +0530

    authenticate

--- frontend/src/auth/helper/index.js
+++ frontend/src/auth/helper/index.js
@@ -5,7 +5,7 @@ export const signup= (user)=> {
     return fetch(`${API}/signup` , {
         method: 'POST', 
         header:{
-            accept: 'application/json',
+            Accept: 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(user)
@@ -16,5 +16,30 @@ export const signup= (user)=> {
     .catch(err=>{ //if there are errors
         console.log(err); 
     })
+}
+
+export const signin= (user)=> {
+    return fetch(`${API}/signin` , {
+        method: 'POST', 
+        header:{
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response=>{ //if success
+        return response.json() // convert the response into json
+    })
+    .catch(err=>{ //if there are errors
+        console.log(err); 
+    })
+}
+
+//authenticate() is a middleware> use next
+export const authenticate= (data, next)=>{
+    if(typeof window != 'undefined'){ //if window object is accessible to us
+        localStorage.setItem('jwt', JSON.stringify(data));
+        next(); 
+    }
 }
 
