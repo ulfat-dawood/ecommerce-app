bcfb1bafd3c380eba453ce340ad15bf130322550
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-06 09:48:30 +0530

    nav items

--- frontend/src/core/menu.js
+++ frontend/src/core/menu.js
@@ -6,11 +6,30 @@ const Menu= ()=>{
         <>
           <ul className="nav nav-tabs bg-dark">
               <li className="nav-item">
-                  <Link className='nav-link'></Link>
+                  <Link className='nav-link' to='/'>HOME</Link>
               </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>CART</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>DASHBOARD</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>A. DASHBOARD</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>SIGN IN</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>SIGN UP</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className='nav-link' to='/'>SIGN OUT</Link>
+              </li>
+
           </ul>
         </>
     )
 }
 
-export default Menu; 
+export default withRouter(Menu); 
