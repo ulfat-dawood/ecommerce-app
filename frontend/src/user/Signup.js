8847f07896e43e5f3a0e7802224a9dfddeeeeb4a
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-06 11:46:13 +0530

    signup form

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -4,9 +4,32 @@ import {Link} from 'react-router-dom';
 import Base from '../core/Base'; 
 
 const Signup=()=>{
+
+    const signupForm= ()=>{
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label  className="text-light">Name:</label>
+                            <input type="text"/>
+
+                            <label  className="text-light">Email:</label>
+                            <input type="email"/>
+
+                            <label  className="text-light">Password</label>
+                            <input type="password"/>
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <Base title='Signup Page' description='A page for user to signup'>
-        <h1>signup please</h1>
+        {signupForm()}
         </Base>
     )
 }
