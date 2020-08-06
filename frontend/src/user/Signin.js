2843db2b8786725a5209120ebbcbb0e92e18e2d0
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-06 11:52:10 +0530

    signin form

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -4,9 +4,29 @@ import {Link} from 'react-router-dom';
 import Base from '../core/Base'; 
 
 const Signin=()=>{
+
+    const signinForm= ()=>{
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label  className="text-light">Email:</label>
+                            <input className="form-control" type="email"/>
+
+                            <label  className="text-light">Password:</label>
+                            <input className="form-control" type="password"/>
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <Base title='Signin Page' description='A page for user to signin'>
-        <h1>signin please</h1>
+            {signinForm()}
         </Base>
     )
 }
--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -12,13 +12,13 @@ const Signup=()=>{
                     <form >
                         <div className="form-group">
                             <label  className="text-light">Name:</label>
-                            <input type="text"/>
+                            <input className="form-control" type="text"/>
 
                             <label  className="text-light">Email:</label>
-                            <input type="email"/>
+                            <input className="form-control" type="email"/>
 
                             <label  className="text-light">Password</label>
-                            <input type="password"/>
+                            <input className="form-control" type="password"/>
                         </div>
                         <button className="btn btn-success btn-block">Submit</button>
                     </form>
