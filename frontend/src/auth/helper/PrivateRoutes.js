cfaeddd46bcba6b74e7cf278d76791b371570f2b
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-19 23:03:31 +0530

    private route

---
+++ frontend/src/auth/helper/PrivateRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+import {Route, Reditect} from 'react-router-dom'; 
+
+import {isAuthenticated} from './index';
+
+function PrivateRoute({ children, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          fakeAuth.isAuthenticated ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
