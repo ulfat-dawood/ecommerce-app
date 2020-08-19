e73d12bba6cd0700512461c733e07e47460bfa3a
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-19 22:50:31 +0530

    conditional rendering the navbar

--- frontend/src/core/menu.js
+++ frontend/src/core/menu.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, {Fragment} from 'react'; 
 import {Link, withRouter} from 'react-router-dom'; 
 
 import {signout, isAuthenticated} from '../auth/helper' //no need for '../auth/helper/index' becase index is the default
@@ -31,12 +31,14 @@ const Menu= (props)=>{
               <li className="nav-item">
                   <Link style={currentTab(history, '/admin/dashboard')} className='nav-link' to='//admin/dashboard'>A. DASHBOARD</Link>
               </li>
-              <li className="nav-item">
+              {!isAuthenticated() && (
+                  <li className="nav-item">
                   <Link style={currentTab(history, '/signin')} className='nav-link' to='/signin'>SIGN IN</Link>
               </li>
               <li className="nav-item">
                   <Link style={currentTab(history, '/signup')} className='nav-link' to='/signup'>SIGN UP</Link>
               </li>
+              )}
               {isAuthenticated() && (
                   <li className="nav-item">
                   <span className='nav-link text-warning' onClick={()=>{
