06b139c46ef5afcf3f829ad7d99185d7eb1168c4
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-20 23:35:14 +0530

    admin dashboard component

---
+++ frontend/src/user/AdminDashBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Base from '../core/Base'; 
+
+const AdminDashboard= ()=>{
+    return(
+        <Base title='Admin Dashboard Page'>
+        <h1>Profile Page</h1>
+        </Base>
+
+    )
+}
+
+export default AdminDashboard; 
