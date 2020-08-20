50acbc565644a3bfdf6b26e02ba9857eb4d96196
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-21 03:18:23 +0530

    structureing admin dashboard

--- frontend/src/user/AdminDashBoard.js
+++ frontend/src/user/AdminDashBoard.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 import Base from '../core/Base'; 
+import {isAuthenticated} from '../auth/helper'; 
 
 const AdminDashboard= ()=>{
+    const myObj= {
+        admin: {name:'john',age:40},
+        user:{name:'jack',age:20}
+    }
+    const {user: {name, email, role}} = myObj; 
+
+    const AdminLeftSide=()=>{
+
+    }
+
+    const AdminRightSide=()=>{
+        
+    }
     return(
-        <Base title='Admin Dashboard Page'>
-        <h1>Profile Page</h1>
+        <Base title='Welcome to admin area' description='manage all of you products here'>
+        {AdminLeftSide()}
+        {AdminRightSide()}
         </Base>
 
     )
