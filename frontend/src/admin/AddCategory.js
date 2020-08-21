0dfb6d069eb03bfce89b7f0052e9546cbd1b713b
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-21 23:22:14 +0530

    create category form

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import Base from '../core/Base'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'; 
+
+import Base from '../core/Base';
+import { isAuthenticated } from '../auth/helper';
 
 const AddCategory= ()=>{
+    const [name, setName]= useState(''); 
+    const [error, setError]= useState(false); 
+    const [success, setSuccess]= useState(false); 
+
+    const {user, token}= isAuthenticated(); 
+
+    const MyCategoryForm=()=>{
+        return(
+            <form>
+                <div className="form-group">
+                    <p className="lead">Enter the category</p>
+                    <input type="text" className='form-control my-3' autoFocus required placeholder='for Ex. summer'/>
+                    <button className="btn btn-outline-info">Create Category</button>
+                </div>
+            </form>
+        )
+    }
     return(
         <Base title='create a category here' description='add a new category for new tshirts'
         className='container bg-info p-4'>
-        
+            <div className="bg-white rounded">
+                <div className="col-md-8 offset-md-2">
+                    {MyCategoryForm()}
+                </div>
+            </div>
         </Base>
 
     )
