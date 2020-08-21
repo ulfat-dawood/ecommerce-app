f14d9042a3a927df16829b1b80ea03b6743eadd3
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-21 22:28:00 +0530

    AddCategory Component

---
+++ frontend/src/admin/AddCategory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddCategory= ()=>{
+    return(
+        <div>
+            <h1>Create a Category</h1>
+        </div>
+
+    )
+}
+
+export default AddCategory; 
