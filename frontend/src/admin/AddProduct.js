6e1808eac65b30c4fd7ece02e9f24b997096687d
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-30 02:48:13 +0530

    creat product form

--- frontend/src/admin/AddProduct.js
+++ frontend/src/admin/AddProduct.js
@@ -1,13 +1,95 @@
 import React from 'react';
+import {Link} from 'react-router-dom'; 
 
 import Base from '../core/Base'; 
 
 const AddProduct=()=>{
 
+    const onSubmit= ()=>{
+
+    }
+
+    const handleChange= name=> event=>{
+        
+    }
+
+    const createProductForm = () => (
+        <form >
+          <span>Post photo</span>
+          <div className="form-group">
+            <label className="btn btn-block btn-success">
+              <input
+                onChange={handleChange("photo")}
+                type="file"
+                name="photo"
+                accept="image"
+                placeholder="choose a file"
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("name")}
+              name="photo"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              onChange={handleChange("description")}
+              name="photo"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("price")}
+              type="number"
+              className="form-control"
+              placeholder="Price"
+              value={price}
+            />
+          </div>
+          <div className="form-group">
+            <select
+              onChange={handleChange("category")}
+              className="form-control"
+              placeholder="Category"
+            >
+              <option>Select</option>
+              <option value="a">a</option>
+              <option value="b">b</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("quantity")}
+              type="number"
+              className="form-control"
+              placeholder="Quantity"
+              value={stock}
+            />
+          </div>
+          
+          <button type="submit" onClick={onSubmit} className="btn btn-outline-success">
+            Create Product
+          </button>
+        </form>
+      );
+
     return(
         <Base title='Add a product here' description='Product creation section'
         className='container text-white bg-info p-4'>
-            <h1>add product</h1>
+            <Link to='/admin/dashboard' className='btn btn-md btn-dark mb-3'>Admin Home</Link>
+            <div className="row bg-dark text-white rounded">
+                <div className="col-md8 offset-md-2">
+                    ho
+                </div>
+            </div>
         </Base>
     )
 }
